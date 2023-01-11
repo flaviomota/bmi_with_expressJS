1c2591251383dee5e23e13522865b712358a7018
fmota89 <fmota@student.42lisboa.com>
2023-01-11 04:07:14 +0000

    server side

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -18,8 +18,21 @@ app.post("/", function (req, res) {
   let result = num1 + num2;
 
   res.send(`The result of the calculation is ${result}`);
-})
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html")
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+
+  let bmi = weight / (height * height);
+
+  res.send(`Your BMI is ${bmi}.`);
+});
 
 app.listen(port, function () {
   console.log(`App listening on port: ${port}`);
-})
+});
